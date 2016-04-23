9aacc193430912ea29cb24984ce153e89a94d89e
eden laboda <eden.laboda@gmail.com>
2016-04-23 15:33:25 +0200

    makes model Task

--- app_todo/models.py
+++ app_todo/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+  description = models.CharField(max_length=100)
+  due_date = models.DateTimeField(verbose_name="Due date")
+  date_crea = models.DateTimeField(auto_now_add=True, auto_now=False,
+                              verbose_name="Date of creation")
+
+  def __str__(self):
+    return self.description
