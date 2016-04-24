9c17bf76a2eba04249339a9d9939b72a9cb18698
eden laboda <eden.laboda@gmail.com>
2016-04-24 21:12:46 +0200

    views CRUD

--- app_todo/views.py
+++ app_todo/views.py
@@ -1,11 +1,38 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import TemplateView,ListView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.forms import ModelForm
 
+from .models import Task
 
 # Create your views here.
-class ListTask(ListView):
-  tasks = Task.objects.all()
 
-class NewTask(CreateView):
-  model = Task
+class TaskForm(ModelForm):
+  class Meta:
+    model = Task
+    fields = ['description', 'due_date']
+
+def home(request, template_name='todo_list/home.html'):
+  tasks = Task.objects.all()
+  return render(request, template_name)
+
+def task_create(request, template_name='todo_list/task_form.html'):
+  form = TaskForm(resquest .POST or None)
+  if form.is_valid():
+    form.save()
+    return redirect('todo_list:home')
+  return render(request, template_name)
+
+def task_update(request, id, template_name='todo_list/task_form.html'):
+  task = get_object_or_404(Task, id=id)
+  form = TaskForm(resquest .POST or None, instance=task)
+  if form.is_valid():
+    form.save()
+    return redirect('todo_list:home')
+  return render(request, template_name)
+
+def task_delete(request, id, template_name='todo_list/task_confirm_delete.html'):
+  task = get_object_or_404(Task, id=id)
+  if request.method=='POST':
+    book.delete()
+    return redirect('books_simple:home')
+  return render(request, template_name)
+
